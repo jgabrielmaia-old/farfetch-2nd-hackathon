1a8ade4a92182d42cde60d4cfcbbd240ea899cae
Pedro <ext.pedro.graca@farfetct.com>
2021-06-16 17:01:07 +0100

    add repos with mongo

---
+++ backend/LookieLooks.Api/LookieLooks.Api/Domain/BsonCollectionAttribute .cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LookieLooks.Api
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
