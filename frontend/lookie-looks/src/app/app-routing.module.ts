353811f68f098cfbdca7bd1b1086b8b6212fdd56
Jonas Gabriel Maia <jgabrielmaia@outlook.com>
2021-06-17 19:27:26 -0300

    challenge page

--- frontend/lookie-looks/src/app/app-routing.module.ts
+++ frontend/lookie-looks/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChallengeComponent } from './pages/challenge/challenge.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/challenge', pathMatch: 'full' },
+   { path: 'challenge', component: ChallengeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
